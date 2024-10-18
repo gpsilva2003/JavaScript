b960673c799cf94ba5c894e431d65ed303946ddc
Gabriel P. Silva <gabriel.silva@gmail.com>
2024-10-18 15:30:20 -0300

    Create simples.js

---
+++ cap8/simples.js
@@ -0,0 +1,8 @@
+   var http = require('http');
+    http.createServer(function(req,res) {
+      res.writeHead(200, { 'Content-Type':
+      'text/plain; charset=utf-8' }); 
+      res.end('Olá mundo!\n');
+    }).listen(3000);
+    console.log('Servidor iniciado em
+    localhost:3000. Ctrl+C para encerrar...');
